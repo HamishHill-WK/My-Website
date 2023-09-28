54e90039ecb2114b9e576e0d80800a7eb98a535d
HamishHill-WK <hamishhill1909@gmail.com>
2023-09-28 16:36:30 +0100

    added basic enemies and collisions

--- JS/SpaceInvaders.js
+++ JS/SpaceInvaders.js
@@ -10,6 +10,8 @@ let bullets = []; // Array to store bullets fired by the player
 let score = 0; // Player's score
 let gameOver = false; // Flag to track if the game is over
 let lastShotTime = 0;
+let shootDelay = 250;
+let enemyDirection = 1;
 
 canvas.backgroundColor = '#000000';
 
@@ -63,6 +65,24 @@ class Bullet extends GameObject {
     }
 }
 
+class SpaceShip extends GameObject {
+    constructor(x, y, width, height, color, dx) {
+        super(x, y, width, height, color);
+        this.dx = dx;
+        this.bullets = [];
+    }
+
+    shoot() {
+        this.bullets.push(new Bullet((this.x + this.width / 2), player.y + 1, 4, 8, '#ff7800', 5));
+    }
+}
+
+class Asteroid {
+    constructor(x, y, width, height, colour) {
+        this.parts = [];
+    }
+}
+
 let player = new GameObject(canvas.width / 2, canvas.height - 60, 40, 40, '#FF0000');
 
 function update() {
@@ -78,11 +98,34 @@ function update() {
         // Draw game elements
         drawGameElements();
 
+        checkCollision();
+
         // Update the score display
         updateScoreDisplay();
     } else {
         // Display game over message or handle game over logic
         // (e.g., show a "Game Over" message, allow the player to restart)
+        restart();
+    }
+}
+
+function checkCollision() {
+    for (let i = aliens.length -1; i >= 0; i--) {
+        for (var j = 0; j < bullets.length; j++) {
+            if (aliens[i] !== undefined && aliens[i].collision(bullets[j])) {
+                aliens.splice(i, 1);
+                bullets.splice(j, 1);
+                score += 10;
+            }
+        }
+    }
+}
+
+function spawnShips() {
+    for (let i = 0; i < 7; i++) {
+        for (let j = 0; j < 5; j++) {
+            aliens.push(new SpaceShip((i * 100) + 10, (j * 50) + 10, 40, 40, '#005e19', enemyDirection));
+        }
     }
 }
 
@@ -93,6 +136,50 @@ function updateScoreDisplay() {
 function updateGameState() {
     for (let i = 0; i < bullets.length; i++) {
         bullets[i].update();
+    }
+
+    if (enemyDirection === 1) {   //if ships are moving right
+        if (aliens.length !== 0) {
+            let closestToRightSideEnemy = aliens[0];
+            for (let i = 0; i < aliens.length; i++) {
+                if (aliens[i].x > closestToRightSideEnemy.x) {
+                    closestToRightSideEnemy = aliens[i];
+                }
+            }
+
+            if (closestToRightSideEnemy.x + closestToRightSideEnemy.width > canvas.width) {
+                enemyDirection = -1;
+                for (let i = 0; i < aliens.length; i++) {
+                    aliens[i].update(0, 10)
+                }
+            }
+
+            for (let i = 0; i < aliens.length; i++) {
+                aliens[i].update(enemyDirection, 0)
+            }
+        }
+    }
+
+    if (enemyDirection === -1) {   //if ships are moving left
+        if (aliens.length !== 0) {
+            let closestToLeftSideEnemy = aliens[0];
+            for (let i = 0; i < aliens.length; i++) {
+                if (aliens[i].x < closestToLeftSideEnemy.x) {
+                    closestToLeftSideEnemy = aliens[i];
+                }
+            }
+
+            if (closestToLeftSideEnemy.x <= 0) {
+                enemyDirection = 1;
+                for (let i = 0; i < aliens.length; i++) {
+                    aliens[i].update(0, 10)
+                }
+            }
+
+            for (let i = 0; i < aliens.length; i++) {
+                aliens[i].update(enemyDirection, 0)
+            }
+        }
     }
 }
 
@@ -106,10 +193,15 @@ function drawGameElements() {
     for (let i = 0; i < bullets.length; i++) {
         bullets[i].draw(ctx);
     }
+
+    for (let i = 0; i < aliens.length; i++) {
+        aliens[i].draw(ctx);
+    }
 }
 
 function init() {
-    canvas.interval = setInterval(update, 1000/ 60);    //set interval of 60 calls per second
+    spawnShips();
+    canvas.interval = setInterval(update, 1000 /  60);    //set interval of 60 calls per second
 }
 
 function keydown() {
@@ -125,7 +217,7 @@ function keydown() {
         }
     }
 
-    if (keys[' '] && currentTime - lastShotTime >= 500) {
+    if (keys[' '] && currentTime - lastShotTime >= shootDelay) {
         bullets.push(new Bullet((player.x + player.width / 2), player.y + 1, 5, 10, '#FFFFFF', -5));
 
         lastShotTime = currentTime;
