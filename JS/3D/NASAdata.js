bb3c7b5b26f6d7177f1aa5ae41fa13b88b40922e
HamishHill-WK <hamishhill1909@gmail.com>
2023-10-30 12:01:55 +0000

    modified request and data procressing functions to handle earth's data, added earth object to scene, refactored init function to use arrays and loops to reduce overall length of the script and clean up code

--- JS/3D/NASAdata.js
+++ JS/3D/NASAdata.js
@@ -1,15 +1,21 @@
 export function request(planetCode) { //this function is used to make a request to the nasa horizon api
 	const format = 'json';
 	const command = `${planetCode}99`;
-	//const command = `399`;	
 	const quantities = '18,19';	//code 18 to request helio-centric longitude and latitude. 19 for The Sun's apparent range, (light-time aberrated) relative to the target center, as seen by the observer, in astronomical units (AU).
 	const startDate = '2023-09-19';
 	const stopDate = '2023-09-20';
-	const url = `https://ssd.jpl.nasa.gov/api/horizons.api?format=${format}&COMMAND=${command}&OBJ_DATA='YES'&MAKE_EPHEM='YES'&EPHEM_TYPE='OBSERVER'&CENTER='500@399'&START_TIME='${startDate}'&STOP_TIME='${stopDate}'&STEP_SIZE='1%20d'&QUANTITIES='${quantities}'`;
+	let observerCode = '399'; //defualt observer earth
+
+	if (planetCode === 3) {//if we are requesting the data for earth we must switch the observer
+		observerCode = '299';
+	}
+
+	const url = `https://ssd.jpl.nasa.gov/api/horizons.api?format=${format}&COMMAND=${command}&OBJ_DATA='YES'&MAKE_EPHEM='YES'&EPHEM_TYPE='OBSERVER'&CENTER='500@${observerCode}'&START_TIME='${startDate}'&STOP_TIME='${stopDate}'&STEP_SIZE='1%20d'&QUANTITIES='${quantities}'`;
+	console.log(url);
 	return fetch(`https://corsproxy.io/?${url}`)	//cors proxy is used to get around cross origin security policy for this api 
 		.then(response => {
-			if (!response.ok) {
-				throw new Error('Network response was not ok');
+			if (!response.ok) {//sometimes get a bad response from api, usually fixed on reloading the page
+				throw new Error('Network response was not ok');	//TODO: handle bad response better 
 			}
 			return response.json();
 		})
@@ -18,14 +24,14 @@ export function request(planetCode) { //this function is used to make a request 
 		});
 }
 
-export function getData(data) {
+export function getData(data) {	//function to extract necessary data from json file received in api request
 	const lines = data.result.split('\n');
 
 	let isInsideBlock = false;
 	const extractedLines = [];
 
 	for (const line of lines) {
-		if (line.trim() === '$$SOE') {
+		if (line.trim() === '$$SOE') {	//the relevant data can be found between between these character codes
 			isInsideBlock = true;
 		} else if (line.trim() === '$$EOE') {
 			isInsideBlock = false;
@@ -35,4 +41,25 @@ export function getData(data) {
 	}
 
 	return extractedLines[0].match(/[-+]?[0-9]*\.?[0-9]+/g);
+}
+
+export function cartesianCoords(longitude, latitude, distance, bodyScale) {	//function to convert heliocentric longitude, latitude and distance from the sun to cartesian coordinates.
+	distance = parseFloat(distance);
+	distance = distance * bodyScale / 500;
+
+	const hEclLonRad = longitude * (Math.PI / 180);
+	const hEclLatRad = latitude * (Math.PI / 180);
+
+	const x = distance * Math.cos(hEclLatRad) * Math.cos(hEclLonRad);
+	const y = distance * Math.cos(hEclLatRad) * Math.sin(hEclLonRad);
+	const z = distance * Math.sin(hEclLatRad);
+
+	const coordinates1 = [x, y, z];
+	const distance1 = Math.sqrt(
+		coordinates1[0] ** 2 + coordinates1[1] ** 2 + coordinates1[2] ** 2
+	);
+
+	console.log(`Distance of coordinates1: ${distance1}, x:${x}, y:${y}, z:${z}`);
+
+	return [x, y, z];
 }
--- JS/3D/main.js
+++ JS/3D/main.js
@@ -9,13 +9,13 @@ document.body.appendChild(renderer.domElement);
 const textureLoader = new THREE.TextureLoader();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100000);
-camera.position.set(0, 0, 25); 
+camera.position.set(0, -25, 0); 
 scene.add(camera);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.listenToKeyEvents(renderer.domElement);
 
-const light = new THREE.PointLight(0xffffff, 1000, 4000);
+const light = new THREE.PointLight(0xffffff, 800, 4000);
 scene.add(light);
 const AmbientLight = new THREE.AmbientLight(0xffffff, 0.1);
 scene.add(AmbientLight);
@@ -79,116 +79,61 @@ class SphereObject {
 }
 
 const texturePath = '../../Images/SolarSystem/';
-const SunTexturePath = `${texturePath}SunTexture.jpg`
-const MercuryTexturePath = `${texturePath}MercuryTexture.jpg`
-const VenusTexturePath = `${texturePath}VenusTexture.jpg`
-const MarsTexturePath = `${texturePath}MarsTexture.jpg`
-const JupiterTexturePath = `${texturePath}JupiterTexture.jpg`
-const SaturnTexturePath = `${texturePath}SaturnTexture.jpg`
-const UranusTexturePath = `${texturePath}UranusTexture.jpg`
-const NeptuneTexturePath = `${texturePath}NeptuneTexture.jpg`
+const SunTexturePath = `${texturePath}SunTexture.jpg`;
 
-const bodyScale = 1000;
-const SunObject = new SphereObject('Sun', 0.465, 128, 64, 0xff0000, SunTexturePath);
-const MercuryObject = new SphereObject('Mercury', 0.01 * bodyScale, 128, 64, 0x0000ff, MercuryTexturePath);
-const VenusObject = new SphereObject('Venus', 0.04 * bodyScale, 128, 64, 0x00ff00, VenusTexturePath);
-//const Earth = new SphereObject(0.042, 32, 16, 0x00ff00);
-const MarsObject = new SphereObject('Mars', 0.022 * bodyScale, 128, 64, 0xff0000, MarsTexturePath);
-const JupiterObject = new SphereObject('Jupiter', 0.4 * bodyScale, 128, 64, 0x550000, JupiterTexturePath);
-const SaturnObject = new SphereObject('Saturn', 0.4 * bodyScale, 128, 64, 0xffff11, SaturnTexturePath);
-const UranusObject = new SphereObject('Uranus', 0.4 * bodyScale, 128, 64, 0xff00ff, UranusTexturePath);
-const NeptuneObject = new SphereObject('Neptune', 0.4 * bodyScale, 128, 64, 0x5500ff, NeptuneTexturePath);
+const bodyScale = 10000;
+const PlanetObjectsArray = [
+	new SphereObject('Mercury', 0.0000163 * bodyScale, 128, 64, 0x0000ff, `${texturePath}MercuryTexture.jpg`),
+	new SphereObject('Venus', 0.0000405 * bodyScale, 128, 64, 0x00ff00, `${texturePath}VenusTexture.jpg`),
+	new SphereObject('Earth', 0.0000426 * bodyScale, 128, 64, 0x00ff00, `${texturePath}EarthTexture.jpg`),
+	new SphereObject('Mars', 0.0000227 * bodyScale, 128, 64, 0xff0000, `${texturePath}MarsTexture.jpg`),
+	new SphereObject('Jupiter', 0.000467 * bodyScale, 128, 64, 0x550000, `${texturePath}JupiterTexture.jpg`),
+	new SphereObject('Saturn', 0.000389 * bodyScale, 128, 64, 0xffff11, `${texturePath}SaturnTexture.jpg`),
+	new SphereObject('Uranus', 0.000169 * bodyScale, 128, 64, 0xff00ff, `${texturePath}UranusTexture.jpg`),
+	new SphereObject('Neptune', 0.000164 * bodyScale, 128, 64, 0x5500ff, `${texturePath}NeptuneTexture.jpg`)
+];
 
-//MercuryObject.setScale(bodyScale, bodyScale, bodyScale);
-//VenusObject.setScale(bodyScale, bodyScale, bodyScale);
-//MarsObject.setScale(bodyScale, bodyScale, bodyScale);
-//JupiterObject.setScale(bodyScale, bodyScale, bodyScale);
-//SaturnObject.setScale(bodyScale, bodyScale, bodyScale);
-//UranusObject.setScale(bodyScale, bodyScale, bodyScale);
-//NeptuneObject.setScale(bodyScale, bodyScale, bodyScale);
-//SunObject.setScale(bodyScale/2, bodyScale/2, bodyScale/2);
-
-MercuryObject.addToScene(scene);
-VenusObject.addToScene(scene);
-MarsObject.addToScene(scene);
-JupiterObject.addToScene(scene);
-SaturnObject.addToScene(scene);
-UranusObject.addToScene(scene);
-NeptuneObject.addToScene(scene);
+//sun object will be at 0,0,0 so no need to include it in the planets array. Planetary objects' coords are calculated relative to the sun. 
+const SunObject = new SphereObject('Sun', 0.004649 * bodyScale / 10, 128, 64, 0xff0000, SunTexturePath);
 SunObject.addToScene(scene);
+
+for (const Planet of PlanetObjectsArray) {
+	Planet.addToScene(scene);
+}
 
 function animate() {
 	requestAnimationFrame(animate);
-
-	//SunObject.setRotation(0, 0, SunObject.mesh.rotation.z + 0.01);
-
 	controls.update();
 	renderer.render(scene, camera);
 }
 
 function init() {
-	const mercury = NASA.request(1);
-	//const earth = request(3);
-	const mars = NASA.request(4);
-	const venus = NASA.request(2);
-	const jupiter = NASA.request(5);
-	const saturn = NASA.request(6);
-	const uranus = NASA.request(7);
-	const neptune = NASA.request(8);
+	const PlanetData = [];
 
-	Promise.all([mercury, venus, mars, jupiter, saturn, uranus, neptune])  //this is used to wait for both api requests to complete
-		.then(data => {
-			const mercuryData = NASA.getData(data[0]);
-			const venusData = NASA.getData(data[1]);
-			//const earthData = getData(data[2]);
-			const marsData = NASA.getData(data[2]);
-			const jupiterData = NASA.getData(data[3]);
-			const saturnData = NASA.getData(data[4]);
-			const uranusData = NASA.getData(data[5]);
-			const neptuneData = NASA.getData(data[6]);
+	for (let i = 1; i < PlanetObjectsArray.length + 1; i++) {
+		PlanetData.push(NASA.request(i));
+	}
 
-			const mercuryPos = cartesianCoords(mercuryData[4], mercuryData[5], mercuryData[6]);
-			const venusPos = cartesianCoords(venusData[4], venusData[5], venusData[6]);
-			//const earthPos = cartesianCoords(earthData[4], earthData[5], earthData[6]);
-			const marsPos = cartesianCoords(marsData[4], marsData[5], marsData[6]);
-			const jupiterPos = cartesianCoords(jupiterData[4], jupiterData[5], jupiterData[6]);
-			const saturnPos = cartesianCoords(saturnData[4], saturnData[5], saturnData[6]);
-			const uransPos = cartesianCoords(uranusData[4], uranusData[5], uranusData[6]);
-			const neptunePos = cartesianCoords(neptuneData[4], neptuneData[5], neptuneData[6]);
+	Promise.all(PlanetData)  //Promise is used to wait for all api requests to complete
+		.then(rawData => {
+			const PlanetsExtractData = [];
+			const PlanetsPositions = [];
+			for (const data of rawData) {
+				PlanetsExtractData.push( NASA.getData(data));	//first relevant data is extracted from the received json file 
+			}
 
-			MercuryObject.setPosition(mercuryPos[0], mercuryPos[1], mercuryPos[2]);
-			VenusObject.setPosition(venusPos[0], venusPos[1], venusPos[2]);
-			//Earth.setPosition(earthPos[0], earthPos[1], earthPos[2]);
-			MarsObject.setPosition(marsPos[0], marsPos[1], marsPos[2]);
-			JupiterObject.setPosition(jupiterPos[0], jupiterPos[1], jupiterPos[2]);
-			SaturnObject.setPosition(saturnPos[0], saturnPos[1], saturnPos[2]);
-			UranusObject.setPosition(uransPos[0], uransPos[1], uransPos[2]);
-			NeptuneObject.setPosition(neptunePos[0], neptunePos[1], neptunePos[2]);
+			for (const extractedData of PlanetsExtractData) {	//the data is then converted in cartesian coords to place the object in the 3D scene
+				PlanetsPositions.push(NASA.cartesianCoords(extractedData[4], extractedData[5], extractedData[6], bodyScale));	
+			}
+
+			for (let i = 0; i < PlanetObjectsArray.length; i++) {	//Rendered planetary objects' positions are updated to correspond with the data received from NASA
+				PlanetObjectsArray[i].setPosition(PlanetsPositions[i][0], PlanetsPositions[i][1], PlanetsPositions[i][2]);
+			}
+
 		})
 		.catch(error => {
 			console.error('Error', error);
 		});
-}
-
-function cartesianCoords(longitude, latitude, distance) {	//function to convert heliocentric longitude, latitude and distance from the sun to cartesian coordinates.
-	distance = parseFloat(distance);
-	distance = distance * bodyScale/2;
-
-	const hEclLonRad = longitude * (Math.PI / 180);
-	const hEclLatRad = latitude * (Math.PI / 180);
-
-	const x = distance * Math.cos(hEclLatRad) * Math.cos(hEclLonRad);
-	const y = distance * Math.cos(hEclLatRad) * Math.sin(hEclLonRad);
-	const z = distance * Math.sin(hEclLatRad);
-
-	const coordinates1 = [x, y, z];
-	const distance1 = Math.sqrt(
-		coordinates1[0] ** 2 + coordinates1[1] ** 2 + coordinates1[2] ** 2
-	);
-
-	console.log(`Distance of coordinates1: ${distance1}, x:${x}, y:${y}, z:${z}`);
-
-	return [x, y, z];
 }
 
 init();
